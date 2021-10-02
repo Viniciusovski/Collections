76b3a980ea40f0e332cb500f050f2a7a9e73de92
unknown <vini98.2012@gmail.com>
2021-10-02 12:37:03 -0300

    exemplo  e metodos de optional

---
+++ Optional/OptionalEstados/src/optional/OptionalEstados.java
@@ -0,0 +1,32 @@
+package optional;
+
+import java.util.Optional;
+
+public class OptionalEstados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Optional<String> optionalString = Optional.of("Valor presente");
+		
+		System.out.println("Valor opcional que está presente");
+		optionalString.ifPresentOrElse(System.out::println, () -> System.out.println("não está presente"));
+		
+		Optional<String> optionalNull = Optional.ofNullable(null);
+		
+		System.out.println("Valor opcional que não está presente");
+		optionalNull.ifPresentOrElse(System.out::println, () -> System.out.println("null = não está presente"));
+		
+		Optional<String> emptyOptional = Optional.empty();
+		
+		System.out.println("Valor opcional que não está presente");
+		emptyOptional.ifPresentOrElse(System.out::println, () -> System.out.println("empty = não está presente"));
+		
+		Optional<String> optionalNullErro = Optional.of(null);
+		System.out.println("Valor opcional que não está presente");
+		optionalNullErro.ifPresentOrElse(System.out::println, () -> System.out.println("erro = não está presente"));
+		
+		
+	}
+
+}
